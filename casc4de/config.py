f410087c4c05ce040f918eed2657ddf90c5813be
nguacon01 <dungdm6191@gmail.com>
2020-10-28 09:58:24 +0100

    update deploy files

--- casc4de/config.py
+++ casc4de/config.py
@@ -2,20 +2,16 @@ import os
 
 class Config(object):
     TESTING = False
-    SECRET_KEY = "X8slQiQWkvC0Zytlrntx9NQB009oOOg5r5kiah68NkckksDyuguwkz0KCV9lK3P5"
+    SECRET_KEY = os.environ["SECRET_KEY"]
     DEBUG = False
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:2361/{db_name}".format(
-    #     username = os.environ["MYSQL_USER"],
-    #     password = os.environ["MYSQL_PASSWORD"],
-    #     host = os.environ["MYSQL_HOST"],
-    #     db_name = os.environ["MYSQL_DATABASE"]
-    # )
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:2361/{db_name}".format(
-        username='mddo',
-        password='dung123',
-        host='192.168.1.16',
-        db_name='casc4de_test'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}".format(
+        username = os.environ["MYSQL_USER"],
+        password = os.environ["MYSQL_PASSWORD"],
+        host = os.environ["MYSQL_HOST"],
+        port = os.environ["MYSQL_PORT"],
+        db_name = os.environ["MYSQL_DATABASE"]
     )
+    
     SQL_TRACK_MODIFICATIONS = False
     JWT_TOKEN_LOCATION = "cookies"
     basedir = os.path.abspath(os.path.dirname(__file__))
@@ -24,7 +20,13 @@ class ProductionConfig(Config):
     pass
 
 class DevelopmentConfig(Config):
-    # print ("base dir is ", Config.basedir)
     TESTING = True
     DEBUG = True
+    SECRET_KEY = "X8slQiQWkvC0Zytlrntx9NQB009oOOg5r5kiah68NkckksDyuguwkz0KCV9lK3P5"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:2361/{db_name}".format(
+        username='mddo',
+        password='dung123',
+        host='192.168.1.16',
+        db_name='casc4de_test'
+    )
     # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(Config.basedir, "site.db")
