018ac55b12b68bfb8de3b8a43e64a868836e5615
nguacon01 <dungdm6191@gmail.com>
2020-10-29 11:42:34 +0100

    create tokenblacklist model

--- casc4de/auth_token/models.py
+++ casc4de/auth_token/models.py
@@ -1,40 +1,6 @@
 from casc4de import db
 import bcrypt
 
-# class User(db.Model):
-#     __tablename__ = "users"
-#     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-#     username = db.Column(db.String(20),nullable = True)
-#     password = db.Column(db.String(200), nullable = False)
-#     email = db.Column(db.String(100), nullable = False, unique = True)
-
-#     def __init__(self, username, password, email):
-#         self.username = username
-#         self.password = bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt())
-#         self.email = email
-    
-#     def __repr__(self):
-#         return self.id, self.username
-
-#     def check_password(self, password):
-#         if bcrypt.checkpw(password.encode('UTF-8'), self.password.encode('UTF-8')):
-#             return True
-#         return False
-#     def set_password(self, password):
-#         self.password = bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
-
-#     def encode_auth_token(self, user_id):
-#         """
-#         Generates the auth token
-#         return string
-#         """
-#     def to_dict(self):
-#         return {
-#             "username" : self.username,
-#             "email" : self.email
-#         }
-
-
 class TokenBlackList(db.Model):
     __tablename__ = "tokenblacklist"
     id = db.Column(db.Integer, nullable = False, primary_key = True, autoincrement=True)
