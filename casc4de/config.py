f448dd2a462504d4441ff9b8a2c7f4888a80bab1
nguacon01 <dungdm6191@gmail.com>
2020-11-03 12:24:45 +0100

    change config

--- casc4de/config.py
+++ casc4de/config.py
@@ -23,10 +23,11 @@ class DevelopmentConfig(Config):
     TESTING = True
     DEBUG = True
     SECRET_KEY = "X8slQiQWkvC0Zytlrntx9NQB009oOOg5r5kiah68NkckksDyuguwkz0KCV9lK3P5"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:2361/{db_name}".format(
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}".format(
         username='mddo',
         password='dung123',
-        host='192.168.1.16',
-        db_name='casc4de_test'
+        host='10.18.0.24',
+        db_name='casc4de_test',
+        port="0601"
     )
     # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(Config.basedir, "site.db")
